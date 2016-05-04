347fce670145fcfea4fc4a79448785369d8a1a25
Daniel Ayala <dmayala001@gmail.com>
2016-05-04 13:50:04 -0700

    Run browserSync setup only once

--- gulp/tasks/dotnet.js
+++ gulp/tasks/dotnet.js
@@ -2,31 +2,34 @@ var gulp = require('gulp');
 var nodemon = require('nodemon');
 var browserSync = require('browser-sync');
 var gutil = require('gulp-util');
+var _ = require('lodash');
 
-gulp.task('dotnet', ['build'], function () {
+var initBrowserSync = _.once(function () {
+  browserSync({
+    proxy: {
+      target: 'localhost:5000',
+      proxyReq: [
+        function (proxyReq) {
+          proxyReq.setHeader('Connection', 'keep-alive');
+        }
+      ]
+    },
+    port: 5001
+  });  
+});
+
+gulp.task('dotnet', function () {
   nodemon({
     watch: 'Controllers',
     ext: 'cs',
     exec: 'dnx web',
     ignore: [ 'node_modules/', 'wwwroot/', 'app/' ]
   })
-  .on('start', function () {
-    browserSync({
-        proxy: {
-            target: 'localhost:5000',
-            proxyReq: [
-                function (proxyReq) {
-                  proxyReq.setHeader('Connection', 'keep-alive');
-                }
-            ]
-        },
-        port: 5001
-    });
-  })
+  .on('start', initBrowserSync)
   .on('restart', function () {
     gutil.log('[Nodemon] ' + gutil.colors.green("'detected change, restarting'") + ' ...');
     setTimeout(function () {
       browserSync.reload({ stream: false });
-    }, 3000);
+    }, 2500);
   });
 });
