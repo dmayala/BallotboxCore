6c8ff30017a4c035d6a6ac0b484b0416a465406e
Daniel Ayala <dmayala001@gmail.com>
2016-05-09 11:10:08 -0700

    Remove unused Identity template controllers, viewmodels, and views.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using Ballotbox.Models;
+using Ballotbox.ViewModels;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Ballotbox.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<BallotboxUser> _userManager;
+        private readonly SignInManager<BallotboxUser> _signInManager;
+        private readonly ILogger _logger;
+
+        public AuthController(
+            UserManager<BallotboxUser> userManager,
+            SignInManager<BallotboxUser> signInManager,
+            ILoggerFactory loggerFactory)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = loggerFactory.CreateLogger<AuthController>();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Login([FromBody] LoginViewModel model)
+        {
+             if (ModelState.IsValid)
+             {
+                var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation(1, "User logged in.");
+                    return Json(new {});
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Json(model);
+                }
+            }
+
+            Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            return Json(new { Message = "Failed", ModelState = ModelState });
+        }
+    }
+}
